032ed74bae91ea48cbe619fec50bdf66cf1e6996
Guillaume <mathieu.guillaume93@gmail.com>
2023-01-30 14:30:37 +0100

    fix repository method

--- src/main/java/com/example/playground/quote/repository/QuoteToTradeRepository.java
+++ src/main/java/com/example/playground/quote/repository/QuoteToTradeRepository.java
@@ -10,9 +10,9 @@ import java.util.List;
 public interface QuoteToTradeRepository extends JpaRepository<QuoteTrade, Long> {
     
     @Query("""
-select trade from QuoteTrade trade
-where trade.status = "ACCEPTED"
-and (trade.userInitiator = :authenticatedUser or trade.userValidator = :authenticatedUser)
-""")
+            select trade from QuoteTrade trade
+            where trade.status = 'ACCEPTED'
+            and (trade.userInitiator = :authenticatedUser or trade.userValidator = :authenticatedUser)
+            """)
     List<QuoteTrade> getAllTradeAcceptedByUser(User authenticatedUser);
 }
