d1fce1f35581ec1b85bcfeca4fa3334beae5a154
Guillaume <mathieu.guillaume93@gmail.com>
2023-04-20 02:19:59 +0200

    useAuthentication hook

---
+++ webapp/quote-reactjs/src/auth/useAuthentication.tsx
@@ -0,0 +1,19 @@
+import {useContext} from "react";
+import UserContext from "./UserContext";
+
+
+export type UseAuthenticationRecord = {
+    isAuthenticated: boolean,
+    isAdmin?: boolean,
+    userName?: string
+}
+
+export function useAuthentication(): UseAuthenticationRecord {
+    const user = useContext(UserContext);
+
+    return {
+        isAuthenticated: !!user,
+        isAdmin: user?.roles?.includes("ADMIN"),
+        userName: user?.userName
+    }
+}
--- webapp/quote-reactjs/src/home/HomeRoutes.tsx
+++ webapp/quote-reactjs/src/home/HomeRoutes.tsx
@@ -1,13 +1,14 @@
 import {Link} from "react-router-dom";
 import React from "react";
+import {useAuthentication} from "../auth/useAuthentication";
 
 export function Home() {
+    const {isAdmin} = useAuthentication();
+
     return <div className={"home-btn-grp"}>
-        {/*<Nav.Link href="/quotedex">Active</Nav.Link>*/}
         <Link to={"/quotedex"} className={"button-17"}>Quotedex</Link>
         <Link to={"/quotes/create"} className={"button-17"}>Inspirer les autres</Link>
         <Link to={"/gifts"} className={"button-17"}>Mes cadeaux</Link>
-        <Link to={"/admin/fetch-rapid-api"} className={"button-17"}>Admin</Link>
-        {/*<button className={"button-17"}>Quotedex</button>*/}
+        {isAdmin && <Link to={"/admin/fetch-rapid-api"} className={"button-17"}>Admin</Link>}
     </div>;
 }
